6deaa6a50fa24155ad6b46f44cdeea605cca6fe8
unknown <Andrew M>
2022-12-02 21:34:27 +0300

    Tried and failed, so let's start from the very beginnig

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.//
+
+
+// Console.WriteLine("Введите первое число:");
+// int first = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int second = Convert.ToInt32(Console.ReadLine());
+
+// if (first>second){
+//     Console.Write($"Первое число ("+first+") - большее, а второе число ("+second+") - меньшее");
+// }
+// else{Console.Write($"Первое число ("+first+") - меньшее, а второе число ("+second+") - большее");
+// }
+
+
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.//
+
+// Console.WriteLine("Введите первое число:");
+// int first = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int second = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число:");
+// int third = Convert.ToInt32(Console.ReadLine());
+// int max = first;
+
+// if (max<second){
+//     max = second;
+// }
+// if (max<third){
+//     max = third;
+// }
+// Console.WriteLine($"Максимальное число равно {max}");
+
+
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).//
+
+// Console.WriteLine("Введите целое число");
+// int numb = Convert.ToInt32(Console.ReadLine());
+
+// if (numb%2 == 0){
+//     Console.WriteLine($"Число {numb} является четным");
+// }
+// else {
+//     Console.WriteLine("Пробуем ещё");
+// }
+
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.//
+
+Console.WriteLine("Введите положительное число:");
+int numb = Convert.ToInt32(Console.ReadLine());
+int t = 0;
+
+while (t <= numb){
+    if (t%2 == 0){
+        Console.WriteLine(t + " ");
+    }
+}
+Console.Write(t + " ");
